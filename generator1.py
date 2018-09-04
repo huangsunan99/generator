391da462f8213a82b8aeacdf9a61a957b6e038d9
huangsunan <hsn@simright.com>
2018-09-04 11:39:31 +0800

    fix generator

--- generator1.py
+++ generator1.py
@@ -1,12 +1,16 @@
 def iter_grid_ids(fpath):
     with open(fpath) as fil:
-        for line in fil.readlines():
-            if line.strip().__contains__('GRID' or 'GRIDS*'):
-                lis = line.strip().split()
+        line = True
+        while line:
+            line = fil.readline()
+            lis = line.strip().split()
+            if len(lis) != 0 and lis[0] == 'GRID*':
+                yield lis[1]
+            elif len(lis) != 0 and lis[0] == 'GRID':
                 yield lis[1]
 
 
 if __name__ == '__main__':
-    bdf = '/home/huangsunan/testmodel/generator-test-model/all_elems.bdf'
+    bdf = '/home/huangsunan/testmodel/generator-test-model/yoke.bdf'
     for grid_id in iter_grid_ids(bdf):
         print grid_id
